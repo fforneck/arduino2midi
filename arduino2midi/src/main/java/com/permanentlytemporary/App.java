0f565d198d87703077c99a012dccce61bc2bb73f
fforneck@gmail.com <fforneck@gmail.com>
2020-05-17 23:55:10 +0000

    init

---
+++ arduino2midi/src/main/java/com/permanentlytemporary/App.java
@@ -0,0 +1,13 @@
+package com.permanentlytemporary;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
