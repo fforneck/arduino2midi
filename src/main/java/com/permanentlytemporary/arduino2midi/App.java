a673d3e1893d01c8496a5ff707506e680da65a59
Filipe Forneck <fforneck@gmail.com>
2020-05-18 23:01:10 -0400

    Update App.java

--- src/main/java/com/permanentlytemporary/arduino2midi/App.java
+++ src/main/java/com/permanentlytemporary/arduino2midi/App.java
@@ -1,11 +1,19 @@
 package com.permanentlytemporary.arduino2midi;
 
+import javax.sound.midi.MidiDevice;
 import javax.sound.midi.MidiEvent;
 import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Receiver;
 import javax.sound.midi.Sequence;
 import javax.sound.midi.Sequencer;
 import javax.sound.midi.ShortMessage;
 import javax.sound.midi.Track;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.fazecast.jSerialComm.SerialPort;
 import com.fazecast.jSerialComm.SerialPortDataListener;
@@ -17,57 +25,99 @@ import com.fazecast.jSerialComm.SerialPortEvent;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+
+    private static final Integer[] POSSIBLE_BAUD_RATES = {300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 28800, 38400, 57600, 115200};
+    private SerialPort serialPort = null;
+    private int baudRate = 57600;
+    private Receiver receiver = null;
+    private Map<Integer, JMenuItem> baudRateMenuMap = new HashMap<Integer, JMenuItem>();
+    private Map<String, MidiDevice> midiDeviceMap = new HashMap<String, MidiDevice>();
+
+    public static void main( String[] args ) {
+
+        App app = new App();
+
+    }
+
+    private void addBaudRateSubMenu(JMenu baudRateMenu, Integer[] rates) {
+        for (Integer rate : rates) {
+            JRadioButtonMenuItem menuItem = new JRadioButtonMenuItem(rate.toString());
+            baudRateMenu.add(menuItem);
+            baudRateMenuMap.put(rate, menuItem);
+        }
+    }
+
+    public App() {
         try {
-            System.out.println( "Hello World!" );
-            System.out.println(SerialPort.getCommPorts());
+            JFrame frame = new JFrame("Arduino to MIDI");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(400,400);
+
+            //Creating the MenuBar and adding components
+            JMenuBar menuBar = new JMenuBar();
+            JMenu portMenu = new JMenu("Port");
+            JMenu baudRateMenu = new JMenu("Baud rate");
+            JMenu midiDeviceMenu = new JMenu("MIDI device");
+            menuBar.add(portMenu);
+            menuBar.add(baudRateMenu);
+            menuBar.add(midiDeviceMenu);
+            addBaudRateSubMenu(baudRateMenu, POSSIBLE_BAUD_RATES);
+
+            // Text Area at the Center
+            JTextArea ta = new JTextArea();
+
+            //Creating the panel at bottom and adding components
+            JPanel panel = new JPanel(); // the panel is not visible in output
+            JLabel label = new JLabel("Enter Text");
+            JTextField tf = new JTextField(10); // accepts upto 10 characters
+            JButton send = new JButton("Send");
+            JButton reset = new JButton("Reset");
+            panel.add(label); // Components Added using Flow Layout
+            panel.add(tf);
+            panel.add(send);
+            panel.add(reset);
+
+            //Adding Components to the frame.
+            frame.getContentPane().add(BorderLayout.NORTH, menuBar);
+            frame.getContentPane().add(BorderLayout.CENTER, ta);
+            frame.getContentPane().add(BorderLayout.SOUTH, panel);
+            frame.setVisible(true);
+
             for (SerialPort serialPort : SerialPort.getCommPorts()) {
-                
-                System.out.println(serialPort.getSystemPortName());
-                System.out.println(serialPort.getDescriptivePortName());
-                System.out.println(serialPort.getPortDescription());
-                System.out.println(serialPort.getBaudRate());
-            }
-    
-            SerialPort serialPort = SerialPort.getCommPort("COM3");
-            serialPort.openPort();
-            serialPort.setBaudRate(57600);
-            serialPort.addDataListener(new SerialPortDataListener() {
-                @Override
-                public int getListeningEvents() { return SerialPort.LISTENING_EVENT_DATA_AVAILABLE; }
-                @Override
-                public void serialEvent(SerialPortEvent event)
-                {
-                   if (event.getEventType() != SerialPort.LISTENING_EVENT_DATA_AVAILABLE)
-                      return;
-                   byte[] newData = new byte[serialPort.bytesAvailable()];
-                   int numRead = serialPort.readBytes(newData, newData.length);
-                   System.out.println("Read " + numRead + " bytes.");
-                   System.out.println(newData[0]);
-                   System.out.println(newData[1]);
-                   System.out.println(newData[2]);
+
+                JRadioButtonMenuItem portMenuItem = new JRadioButtonMenuItem(serialPort.getSystemPortName() + " - " + serialPort.getDescriptivePortName());
+                portMenu.add(portMenuItem);
+                if ("COM3".equals(serialPort.getSystemPortName())) {
+                    this.serialPort = serialPort;
+                    portMenu.setSelected(true);
+                    baudRate = serialPort.getBaudRate();
+                    baudRateMenuMap.get(baudRate).setSelected(true);
+                    openSerialPort("COM3", ta);
                 }
-            });
-    
-            Sequencer sequencer = MidiSystem.getSequencer();
-            sequencer.open();
-            Sequence sequence = new Sequence(Sequence.PPQ, 4);
-            Track track = sequence.createTrack();
-            track.add(makeEvent(144, 1, 48, 100, 48));
-            track.add(makeEvent(128, 1, 48, 100, 50));
-            sequencer.setSequence(sequence);
-            sequencer.setTempoInBPM(220);
-            sequencer.start();
-  
-            while (true) { 
-  
-                // Exit the program when sequencer has stopped playing. 
-                if (!sequencer.isRunning()) { 
-                    sequencer.close(); 
-                    System.exit(1); 
-                } 
-            } 
+            }
+            
+            for (MidiDevice.Info deviceInfo : MidiSystem.getMidiDeviceInfo()) {
+                MidiDevice device = MidiSystem.getMidiDevice(deviceInfo);
+                if (device.getMaxReceivers() != 0) {
+                    JRadioButtonMenuItem midiDeviceMenuItem = new JRadioButtonMenuItem(deviceInfo.getName());
+                    midiDeviceMenu.add(midiDeviceMenuItem);
+                    if ("LoopBe Internal MIDI".equals(deviceInfo.getName())) {
+                        midiDeviceMenuItem.setSelected(true);
+                        receiver = device.getReceiver();
+                    }
+                    midiDeviceMenuItem.addActionListener(new ActionListener(){
+                        public void actionPerformed(ActionEvent event) {
+                            try {
+                                receiver = device.getReceiver();
+                            } catch (Exception error) {
+                                System.out.println("Error: " + error.getMessage());
+                                error.printStackTrace();
+                                System.exit(1);
+                            }
+                        }
+                    });
+                }
+            }
 
             
         } catch (Exception e) {
@@ -75,6 +125,37 @@ public class App
             System.err.println(e.getMessage());
             e.printStackTrace();
         }
+    }
+
+    private void openSerialPort(String portName, JTextArea loggingArea) {
+        if (serialPort != null && serialPort.isOpen()) {
+            serialPort.closePort();
+        }
+    
+        SerialPort serialPort = SerialPort.getCommPort("COM3");
+        serialPort.openPort();
+        serialPort.setBaudRate(57600);
+        serialPort.addDataListener(new SerialPortDataListener() {
+            @Override
+            public int getListeningEvents() { return SerialPort.LISTENING_EVENT_DATA_AVAILABLE; }
+            @Override
+            public void serialEvent(SerialPortEvent event)
+            {
+               if (event.getEventType() != SerialPort.LISTENING_EVENT_DATA_AVAILABLE)
+                  return;
+               byte[] newData = new byte[serialPort.bytesAvailable()];
+               int numRead = serialPort.readBytes(newData, newData.length);
+               
+               System.out.println("Read " + numRead + " bytes.");
+               System.out.println(newData[0]);
+               System.out.println(newData[1]);
+               System.out.println(newData[2]);
+               int sensor = ByteBuffer.wrap(subArray(newData, 0, 1)).getInt();
+               int velocity = ByteBuffer.wrap(subArray(newData, 1, 2)).getInt();
+               loggingArea.append("Sensor " + sensor + " Velocity " + velocity);
+            }
+        });
+
     }
   
     public static MidiEvent makeEvent(int command, int channel, 
@@ -100,4 +181,12 @@ public class App
         } 
         return event; 
     } 
+
+    private static byte[] subArray(byte[] input, int start, int length) {
+        byte[] ret = new byte[length];
+        for (int i = 0; i < length; i++) {
+            ret[i] = input[start + i];
+        }
+        return ret;
+    }
 }
